a5815bdef9808cc4008eebb73431ad071151db27
AndreiZaretski <105275676+AndreiZaretski@users.noreply.github.com>
2023-05-07 01:55:37 +0300

    feat: add random data on every flight

--- index.js
+++ index.js
@@ -21,85 +21,67 @@ app.use(bodyParser.json());
 app.post('/getairs', (req, res)=>{
  const {from, to, way, endDate, startDate, passengersCount } = req.body; 
 
- let getDay;
- let getDayBack;
+//  let getDay;
+//  let getDayBack;
 
- const startDay = new Date(startDate).getDay();
- const endDay = new Date(endDate).getDay();
+//  const startDay = new Date(startDate).getDay();
+//  const endDay = new Date(endDate).getDay();
 
- const date = new Date(endDate)- new Date(startDate);
- const arrDate = mockData.days[helpers.getRandomElementArray(mockData.days)];
- const arrDateBack = mockData.days[helpers.getRandomElementArray(mockData.days)];
+//  const date = new Date(endDate)- new Date(startDate);
+//  const arrDate = mockData.days[helpers.getRandomElementArray(mockData.days)];
+//  const arrDateBack = mockData.days[helpers.getRandomElementArray(mockData.days)];
 
- const available = helpers.getRandomNumberPassengers(passengersCount);
- const availableBack = helpers.getRandomNumberPassengers(passengersCount);
-
- const price = mockData.price[helpers.getRandomElementArray(mockData.price)];
- const priceBack = mockData.price[helpers.getRandomElementArray(mockData.price)];
-
- const timeWay = mockData.timeWay[helpers.getRandomElementArray(mockData.timeWay)];
- const timeWayBack = mockData.timeWay[helpers.getRandomElementArray(mockData.timeWay)];
-
- const flightNumber = mockData.flightNumber+Math.floor(Math.random() * 9999);
- const flightNumberBack = mockData.flightNumber+Math.floor(Math.random() * 9999);
-
- const direct = mockData.direct[helpers.getRandomElementArray(mockData.direct)];
- const directBack = mockData.direct[helpers.getRandomElementArray(mockData.direct)];
-
- let airportRedirect;
- if(direct===true) {
-  airportRedirect = [[''], ['']];
- }
- if(direct===false) {
-  airportRedirect = mockData.airportRedirect[helpers.getRandomElementArray(mockData.airportRedirect)];
-  ;
- }
- let airportRedirectBack;
- if(directBack===true) {
-  airportRedirectBack = [[''], ['']];
- }
- if(directBack===false) {
-  airportRedirectBack = mockData.airportRedirect[helpers.getRandomElementArray(mockData.airportRedirect)];
-  ;
- }
-
- const startTime = mockData.startTime[helpers.getRandomElementArray(mockData.startTime)];
- const startTimeBack = mockData.startTime[helpers.getRandomElementArray(mockData.startTime)];
-
+ const available =()=> helpers.getRandomNumberPassengers(passengersCount);
  
-
-
+ const price =()=> mockData.price[helpers.getRandomElementArray(mockData.price)];
  
-
+ const timeWay =()=>mockData.timeWay[helpers.getRandomElementArray(mockData.timeWay)];
  
+ const flightNumber = ()=>{return mockData.flightNumber+Math.floor(Math.random() * 9999)};
 
- if (date >=604800000) {
-  getDay = arrDate;
-  getDayBack = arrDateBack;
- } else {
-  getDay = helpers.filterArray([startDay, endDay].sort((a,b) =>a-b), arrDate);
-  getDayBack = helpers.filterArray([startDay, endDay].sort((a,b) =>a-b), arrDateBack);
-   if(getDay.length === 0) {
-    getDay = [startDay];
+ const direct = ()=> {const isDirect = mockData.direct[helpers.getRandomElementArray(mockData.direct)];
+  let airportRedirect;
+  if(isDirect===true) {
+    airportRedirect = [[''], ['']];
    }
-   if(getDayBack.length === 0) {
-    getDayBack = [startDay];
+   if(isDirect===false) {
+    airportRedirect = mockData.airportRedirect[helpers.getRandomElementArray(mockData.airportRedirect)];
+    ;
    }
- }
-
+   return {isDirect, airportRedirect}
+};
  
+ const startTime =()=> mockData.startTime[helpers.getRandomElementArray(mockData.startTime)];
+ 
+//  if (date >=604800000) {
+//   getDay = arrDate;
+//   getDayBack = arrDateBack;
+//  } else {
+//   getDay = helpers.filterArray([startDay, endDay].sort((a,b) =>a-b), arrDate);
+//   getDayBack = helpers.filterArray([startDay, endDay].sort((a,b) =>a-b), arrDateBack);
+//    if(getDay.length === 0) {
+//     getDay = [startDay];
+//    }
+//    if(getDayBack.length === 0) {
+//     getDayBack = [startDay];
+//    }
+//  }
 
- const thereWay = {startTime, timeWay, flightNumber, price, available, getDay, direct, airportRedirect};
+ const isFlight = ()=>  mockData.isFlight[helpers.getRandomElementArray(mockData.isFlight)]
 
- const backWay = {startTimeBack, timeWayBack, flightNumberBack, priceBack, availableBack, getDayBack, directBack, airportRedirectBack};
+ const wayFunc =  () =>{return {startTime: startTime(), timeWay: timeWay(), flightNumber:flightNumber(),  price: price(),  available: available(), isFlight: isFlight(), direct: direct()}};
+
+ const thereWay = [ wayFunc(), wayFunc(), wayFunc(),wayFunc(),wayFunc(),wayFunc(),wayFunc(),wayFunc(),wayFunc(), wayFunc()]
+
+ const backWay = [ wayFunc(), wayFunc(), wayFunc(),wayFunc(),wayFunc(),wayFunc(),wayFunc(),wayFunc(),wayFunc(), wayFunc()];
 
  if(way==='one-way') {
-  return res.json({from, to, way, thereWay});
+  return res.json({from, to, way, endDate, startDate, thereWay});
  }
 
 
 if(way==='round') {
-  return res.json({from, to, way, thereWay, backWay});
+  return res.json({from, to, way, endDate, startDate, thereWay, backWay});
 }
 
 });
